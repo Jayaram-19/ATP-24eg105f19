4711a91abcb060c5d77baed2dd104c7e841e9493
Jayaram Aditya <24eg105f19@anurag.edu.in>
2026-05-14 14:32:27 +0530

    Rename big2.js to BiggestofTwoNumbers.js

---
+++ week-1/BiggestofTwoNumbers.js
@@ -0,0 +1,3 @@
+let x=10;
+let y=20;
+console.log((x>y)?(`x is big its value is ${x}`):(`y is big its value is ${y}`));
--- week-1/big2.js
+++
@@ -1,3 +0,0 @@
-let x=10;
-let y=20;
-console.log((x>y)?(`x is big its value is ${x}`):(`y is big its value is ${y}`));
