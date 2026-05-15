eea699990733a29a658515b493da97d5f726d053
Jayaram Aditya <24eg105f19@anurag.edu.in>
2026-05-15 14:04:17 +0530

    Add comments for clarity in BiggestofTwoNumbers.js
    Added comments to explain variable declaration and comparison.

--- week-1/BiggestofTwoNumbers.js
+++ week-1/BiggestofTwoNumbers.js
@@ -1,3 +1,5 @@
+//decalring a variable and assigning a value to it
 let x=10;
 let y=20;
+//comparing the variable values and printing the biggest value 
 console.log((x>y)?(`x is big its value is ${x}`):(`y is big its value is ${y}`));
