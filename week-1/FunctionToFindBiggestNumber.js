2943fc3a9a6a596cbb957ec39115a84c5ca2e286
Jayaram Aditya <24eg105f19@anurag.edu.in>
2026-05-15 14:09:58 +0530

    Update FunctionToFindBiggestNumber.js

--- week-1/FunctionToFindBiggestNumber.js
+++ week-1/FunctionToFindBiggestNumber.js
@@ -1,5 +1,8 @@
+//declaring a function named big
 function big(x,y,z){
+//finding biggest value by using ternary operator
 let u=(x>y&&x>z)?(`x is big its value is ${x}`):(y>z)?(`y is big its value is ${y}`):(`z is big its value is ${z}`)
 return u;
 }
+//printing biggest value
 console.log(big(10,20,30));
