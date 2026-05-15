f00bd3f46d1313dd69974e847156549466b24639
Jayaram Aditya <24eg105f19@anurag.edu.in>
2026-05-15 14:11:09 +0530

    Update FunctionToFindSumOfArray.js

--- week-1/FunctionToFindSumOfArray.js
+++ week-1/FunctionToFindSumOfArray.js
@@ -1,3 +1,4 @@
+//declaring a function for finding sum of elements in an array
 function total(arr){
 let t1=0
 for(let i=0;i<arr.length;i++){
@@ -6,4 +7,5 @@ t1+=arr[i];
 return `sum of the elements in the array is ${t1}`;
 }
 let arr=[10,30,20];
+//printing after calling the function
 console.log(total(arr));
