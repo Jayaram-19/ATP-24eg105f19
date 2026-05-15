9dc380e52b3b087fcd46759f6f913e965ae459ea
Jayaram Aditya <24eg105f19@anurag.edu.in>
2026-05-15 14:13:15 +0530

    Update SumOfElementsInArray.js

--- week-1/SumOfElementsInArray.js
+++ week-1/SumOfElementsInArray.js
@@ -1,6 +1,9 @@
+//declaring an array
 let marks=[90,78,65,98];
+//initializing with zero to add all elements
 let sum=0;
 for(let i=0;i<marks.length;i++){
 sum+=marks[i]
 }
+//printing out the sum
 console.log(`sum of the elements in the array is ${sum}`);
